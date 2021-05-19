47c276c867eca5269e987b43d8f0246412427449
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 23:12:45 -0400

    Add project files.

---
+++ DesignPattern_SingletonThreadSafeEagerLoading/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_SingletonThreadSafeEagerLoading
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        private static readonly Singleton singleInstance = new Singleton();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return singleInstance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
